44abb41d84b8794bc48675336fd8f82258e1b2ad
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-07-21 22:30:09 +0900

    配色の設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,22 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        seenceofplace: {
+          "primary": "#BFE0CD",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#F6F0D0",
+          "info": "#C6E4EC",
+          "success": "#62CB93",
+          "warning": "#CBB162",
+          "error": "#B03B3B",
+          "text-color": "#7E615B"
+        },
+      },
+    ],
+  }
 }
