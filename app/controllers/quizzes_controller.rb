9a122dad69d4924fae564d1dac47081f74f48b8a
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-08-09 21:07:04 +0900

    クイズの選択肢を追加

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,12 +3,16 @@ class QuizzesController < ApplicationController
   def new
     # ランダムに並び替え、そのうち2件を取得する
     @locations = Location.order("RANDOM()").limit(2)
-    # calculate_distanceのメソッドに対して上記で取得した2地点を引数として渡す
-    @distance = calculate_distance(@locations[0], @locations[1])
-
     # Javascriptに渡せるように設定
     gon.location1 = @locations[0]
     gon.location2 = @locations[1]
+
+    # calculate_distanceのメソッドに対して上記で取得した2地点を引数として渡す
+    @distance = calculate_distance(@locations[0], @locations[1])
+
+    # generate_choicesメソッドにAPIで計算した正答な距離を引数として@choicesに代入
+    @choices = generate_choices(@distance)
+
   end
 
   def show;end
@@ -32,4 +36,31 @@ class QuizzesController < ApplicationController
     distance_in_km = data["rows"][0]["elements"][0]["distance"]["value"] / 1000.0
     distance_in_km.round(1)
   end
+
+
+  # 選択肢を生成するロジック
+  def generate_choices(distance)
+
+    # distanceに基づいてダミーデータの範囲を設定(±30%)
+    variation = (distance * 0.3).round
+
+    # distanceを基準としたダミーデータを4つ作成する。整数で出力。
+    dummy_distances = [
+      (distance + rand(variation..(variation * 2))).to_i,
+      (distance + rand(variation..(variation * 2))).to_i,
+      (distance - rand(variation..(variation * 2))).to_i,
+      (distance - rand(variation..(variation * 2))).to_i
+    ]
+
+    # 上記のdummy_distanceから0以上の値を条件に取り出す
+    valid_distances = dummy_distances.select { |d| d >= 0 }
+
+    # 4つのダミーデータから2つをランダムで選択し正答と合わせてchoiceに代入する
+    selected_dummies = valid_distances.sample(2)
+    choices = [distance.to_i] + selected_dummies
+
+    # 要素を並び替える
+    choices.shuffle
+  end
+
 end
