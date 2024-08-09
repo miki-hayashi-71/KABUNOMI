aedefa1626cb5fb79dcdb5f7074684a028c65308
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-08-09 14:06:51 +0900

    distance_matrix_apiの実装

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,35 @@
 class QuizzesController < ApplicationController
 
-  def new; end
+  def new
+    # ランダムに並び替え、そのうち2件を取得する
+    @locations = Location.order("RANDOM()").limit(2)
+    # calculate_distanceのメソッドに対して上記で取得した2地点を引数として渡す
+    @distance = calculate_distance(@locations[0], @locations[1])
 
-  def show; end
+    # Javascriptに渡せるように設定
+    gon.location1 = @locations[0]
+    gon.location2 = @locations[1]
+  end
 
+  def show;end
+
+  private
+
+  # 距離計算ロジック location1とlocation2は、newメソッドの@locations[0]と@locations[1]とイコール
+  def calculate_distance(location1, location2)
+    # 出発地点と到着地点の緯度経度をそれぞれoriginとdestinationに代入
+    origin = "#{location1.latitude},#{location1.longitude}"
+    destination = "#{location2.latitude},#{location2.longitude}"
+
+    api_key = ENV['MAPS_JAVASCRIPT_API']
+
+    # APIエンドポイントに対して2点間の距離を計算するための情報を渡す
+    url = URI("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=#{origin}&destinations=#{destination}&key=#{api_key}")
+    response = Net::HTTP.get(url)
+    data = JSON.parse(response)
+
+    # APIから帰ってきたdataから必要な情報を取り出し、単位kmに変換する
+    distance_in_km = data["rows"][0]["elements"][0]["distance"]["value"] / 1000.0
+    distance_in_km.round(1)
+  end
 end
