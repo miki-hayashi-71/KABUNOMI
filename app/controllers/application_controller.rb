1407224fc947834efef8dd652a08cba655dcb365
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-08-23 23:39:25 +0900

    フラッシュメッセージの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  # before_action :require_login
-
-  # private
-
-  # def not_authenticated
-  #   redirect_to root_path
-  # end
+  add_flash_types :success, :danger
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ユーザー登録に成功しました"
       redirect_to root_path
     else
-      render :new
+      flash.now[:error] = "ユーザー登録に失敗しました"
+      render :new, status: :unprocessable_entity
     end
   end
 
