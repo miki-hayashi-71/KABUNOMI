bb853ec79d018725f8ab8f1d83c5758b574197bd
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-09-14 14:53:13 +0900

    ユーザー情報の変更機能

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,9 +1,31 @@
 class MypagesController < ApplicationController
 
+  before_action :set_user, only: %i[edit update]
+
   def edit; end
 
-  def update;end
+  # 更新
+  def update
+    if @user.update(user_params)
+      flash[:success] = t('mypages.edit.success', item: User.model_name.human)
+      redirect_to mypage_path
+    else
+      flash.now[:error] = t('mypages.edit.failure', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def show; end
 
+  private
+
+  # ログイン中のユーザーのidを取得し、@userに代入
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  # userから各データを受け取ったパラメータ
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
 end
