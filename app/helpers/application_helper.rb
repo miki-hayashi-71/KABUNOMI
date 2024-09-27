6c080c2f994ad0a7da401b0e3f086f7c0114134e
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-09-27 21:51:19 +0900

    OGPの本番環境対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,20 +17,20 @@ module ApplicationHelper
       charset: 'utf-8', # HTMLの文字エンコーディング
       description: '2地点間の距離を3択から選ぶクイズアプリです。',  # ページの説明 検索エンジンやSNSで表示される場合有り
       keywords: 'クイズアプリ,地図クイズ,距離クイズ,googlemap,距離感',  # ページのキーワード
-      canonical: 'https://sence-of-distance.com/', # ページの正規URL
+      canonical: 'https://sence-of-distance.com/ranking ', # ページの正規URL
       separator: '|',
       og: { # Open Graphプロトコルのためのメタタグ情報
         site_name: :site,
         title: :title,
         description: :description,
         type: 'website',
-        url: 'https://sence-of-distance.com/',
+        url: 'https://sence-of-distance.com/ranking ',
         image: image_url('OGP_image.png'),# 配置するパスやファイル名によって変更
         local: 'ja-JP',
       },
       twitter: { # Twitterカードのためのメタタグ情報
         card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
-        site: '@kin_doo_nichi', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
+        site: '@https://x.com/kin_doo_nichi', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
         image: image_url('OGP_image.png'),# カードで表示される画像。配置するパスやファイル名によって変更
       }
     }
