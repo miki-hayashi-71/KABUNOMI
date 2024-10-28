23f70ce8014da1d2bc05277165811ee150051b21
SOYODAMO <soyo.damo.mk71@gmail.com>
2024-10-28 23:00:29 +0900

    fix:sorceryによるgoogleログイン

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,14 +1,7 @@
 class OauthsController < ApplicationController
-  # gem googleauthのg_csrf_tokenの検証ライブラリの読み込み
-  require 'googleauth/id_tokens/verifier'
-
-  # callbackアクションでのCSRF保護を無効化。これがないと外部からのリクエストのため弾かれる
-  protect_from_forgery except: :callback
 
   # ログイン不要
   skip_before_action :require_login
-
-  before_action :verify_g_csrf_token
 
   def oauth
     #指定されたプロバイダの認証ページにユーザーをリダイレクトさせる
@@ -17,14 +10,14 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-    # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
-    if (@user = login_from(provider))
-      redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
+    @user = login_from(provider)
+    if (@user)
+      redirect_to root_path, success:"#{provider.titleize}アカウントでログインしました"
     else
       begin
         # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
         signup_and_login(provider)
-        redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
+        redirect_to root_path, success:"#{provider.titleize}アカウントでログインしました"
       rescue
         redirect_to root_path, alert:"#{provider.titleize}アカウントでのログインに失敗しました"
       end
@@ -43,10 +36,4 @@ class OauthsController < ApplicationController
     auto_login(@user)
   end
 
-  # クッキーに保存されたg_csrf_tokenとリクエストパラメータ内のg_csrf_tokenが一致するかを検証
-  def verify_g_csrf_token
-    if cookies["g_csrf_token"].blank? || params[:g_csrf_token].blank? || cookies["g_csrf_token"] != params[:g_csrf_token]
-      redirect_to root_path, notice: '不正なアクセスです'
-    end
-  end
 end
